a1d6fa3bafece2eba6950f0d9da49cf3d92e1b61
kawsar-ahmmed <kawsarahmmedr@gmail.com>
2023-05-18 05:21:26 +0600

    Final product card added

---
+++ src/components/Header/Header.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import logo from '../../images/Logo.svg';
+import './Header.css';
+import { Link } from 'react-router-dom';
+
+const Header = () => {
+    return (
+        <nav className='header'>
+            <img src={logo} alt="" />
+            <div>
+                {/* <Link to={}></Link> */}
+                <Link to="/shop">Shop</Link>
+                <Link to="/orders">Orders</Link>
+                <Link to="/inventory">Inventory</Link>
+                <Link to="/about">About</Link>
+            </div>
+        </nav>
+    );
+};
+
+export default Header;
